142a239cbe7dfb1ff5ef6214f46ca76c4c649129
ciyek <aripurnomo606@gmail.com>
2024-10-28 12:24:17 +0700

    UpdateProgress🚀

--- rent-office-fe/src/pages/Browse.tsx
+++ rent-office-fe/src/pages/Browse.tsx
@@ -6,10 +6,10 @@ export default function Browse() {
         <>
   <nav className="bg-white">
     <div className="flex items-center justify-between w-full max-w-[1130px] py-[22px] mx-auto">
-      <a href="index.html">
+      <a href="/">
         <img src="/assets//images/logos/logo.svg" alt="logo" />
       </a>
-      <ul className="flex items-center gap-[50px] w-fit">
+      <ul className="flex items-center font-bold gap-[50px] w-fit">
         <li>
           <a href="">Browse</a>
         </li>
